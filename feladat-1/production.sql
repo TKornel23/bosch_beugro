edab5d2035dda564cee22fe9a1ce75f7818e7478
Kornél Tóth <tkornel23@gmail.com>
2022-03-31 13:12:18 +0200

    Production and Products tables created

---
+++ feladat-1/production.sql
@@ -0,0 +1,9 @@
+USE cs_beugro;
+CREATE TABLE `production` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pcb_id` INT NOT NULL,
+  `quantity` INT,
+  `startDate` DATE,
+  `endDate` DATE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
