a949604175b943c50a03027cdadc73da145c6183
Kornél Tóth <tkornel23@gmail.com>
2022-03-31 15:03:19 +0200

    Production Logic: Collection setup

---
+++ feladat-1/ProductionSimulation/ProductionSimulation/Logic/ProductionLogic.cs
@@ -0,0 +1,25 @@
+﻿using ProductionSimulation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionSimulation.Logic
+{
+    public class ProductionLogic
+    {
+        public List<Products> Products { get; set; }
+        private cs_beugroContext _ctx;
+
+        public ProductionLogic(cs_beugroContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+        private void SetupProducts()
+        {
+            this.Products = _ctx.Products.ToList<Products>();
+        }
+    }
+}
---
+++ feladat-1/ProductionSimulation/ProductionSimulation/Logic/RandomGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionSimulation.Logic
+{
+    static class RandomGenerator
+    {
+        static Random rnd = new Random();
+    }
+}
