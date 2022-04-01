f63d3eb80973ab3dee4ff21f72de1509c8145ca1
Kornél Tóth <tkornel23@gmail.com>
2022-04-01 11:21:42 +0200

    jquery color changer, jquery text changer

---
+++ feladat-2/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+
+<div >
+    <p>Kornél Tóth</p>
+    <p>tkornel23@gmail.com</p>
+</div>
+
+@endsection
--- feladat-2/resources/views/center.blade.php
+++ feladat-2/resources/views/center.blade.php
@@ -1,15 +1,36 @@
+@extends('layouts.app')
+@section('content')
+
 <div class="card" id="card" style="width: 18rem;">
   <div class="card-body">
-    <p class="card-text" id="text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text" id="text">asfkyvmxoicvjxoicbv54515</p>
+    <button id="button">Change password to *</button>
   </div>
 </div>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>
 
-    $('#text').on('mouseover',function(){
+    $('#text').on('click',function(){
         var color = '#'+Math.floor(Math.random()*16777215).toString(16);
         $('#text').css('background-color',color);
+
+        var result ='';
+        var characterList ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for(var i = 0; i < 20; i++){
+            result += characterList.charAt(Math.floor(Math.random() * characterList.length));
+        }
+        $('#text').text(result);
     })
 
+    $('#button').on('click',function(){
+        var p = $('#text').text();
+        var result = '';
+        for(var i = 0; i < p.length; i++){
+            result += '*';
+        }
+        $('#text').text(result);
+    })
 </script>
 
+@endsection
